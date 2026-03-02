cdf4edfe7ba453ac3238768eae7608c06bc6fdb9
tuksido <tutuksido@hotmail.com>
2026-03-02 08:49:43 -0500

    Fix: production dependencies and port config

--- server.ts
+++ server.ts
@@ -73,7 +73,7 @@ try {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json({ limit: '50mb' }));
   app.use(cookieParser());
